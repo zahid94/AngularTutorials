e869305468b63057dc044977015043345ff808f2
zahidul islam <zahidulislam5594@gmail.com>
2021-04-25 00:35:48 +0600

    Wildcard Route and Redirecting Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 import { EmployeedetailsComponent } from './Employee/employeedetails/employeedetails.component';
 import { EmployeelistComponent } from './Employee/employeelist/employeelist.component';
+import { PageNotFountComponent } from './Error/page-not-fount/page-not-fount.component';
 
 const routes: Routes = [
+  {path:"",redirectTo:"/employeelist", pathMatch:"full"},
+  
   {path:"employeelist",component:EmployeelistComponent},
-  {path:"employeedetails",component:EmployeedetailsComponent}
+  {path:"employeedetails",component:EmployeedetailsComponent},
+
+  {path:"**",component:PageNotFountComponent}
 ];
 
 @NgModule({
@@ -13,4 +19,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const RoutingComponent=[EmployeelistComponent,EmployeedetailsComponent]
+export const RoutingComponent=[EmployeelistComponent,EmployeedetailsComponent,PageNotFountComponent]
